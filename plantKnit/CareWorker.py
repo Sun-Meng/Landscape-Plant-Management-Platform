08027484b30c8090f88c777cba21eacec1732cc2
Sun-Meng <sunmeng@bjfu.edu.cn>
2023-12-19 10:45:35 +0800

    12.19 10:45 sunmeng hand_in his work

---
+++ plantKnit/CareWorker.py
@@ -0,0 +1,8 @@
+class CareWorker(object):
+    def __init__(self, ID, Name, Sex, Birth, Tel):
+        self.__ID = ID
+        self.__Name = Name
+        self.__Sex = Sex
+        self.__Birth = Birth
+        self.__Tel = Tel
+    
