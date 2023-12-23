1f9895bdc0958db785f068dbaeb7dfb2378c7971
Xi-Haisen <1184249103@qq.com>
2023-12-23 12:17:38 +0800

    domain.Xi 第一次更改

---
+++ db/domain/Xi/Monitor.py
@@ -0,0 +1,6 @@
+class Monitor:
+    def __init__(self, diseaseID, name, create_time, update_time):
+        self.diseaseID = diseaseID
+        self.name = name
+        self.create_time = create_time
+        self.update_time = update_time
---
+++ db/domain/Xi/Monitoring_Equipment.py
@@ -0,0 +1,6 @@
+class Monitoring_Equipment:
+    def __init__(self, id, time, place, object):
+        self.id = id
+        self.time = time
+        self.place = place
+        self.object = object
