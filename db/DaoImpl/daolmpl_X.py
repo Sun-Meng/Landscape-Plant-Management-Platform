92acbad6696c302b28e2037aaa9ec9efa3d278b1
Xi-Haisen <1184249103@qq.com>
2023-12-23 14:58:48 +0800

    第一次修改

--- db/DaoImpl/daolmpl_X.py
+++ db/DaoImpl/daolmpl_X.py
@@ -1,10 +1,116 @@
-from dao import dao
+from db.Dao.dao_X import Monitor_dao, Monitoring_Equipment_dao, Monitoring_Personnel_dao
 
-class MonitorDao(dao):
-    pass
+from db.utils.dao import base_dao
 
-class Monitoring_EquipmentDao(dao):
-    pass
+class Monitor_dao_Impl(base_dao,Monitor_dao):
+    
+    def _init__(self):
+        self.connection = self.get_conn()
 
-class Monitoring_PersonnelDao(dao):
-    pass
+    def insert(self,Monitor) :
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+
+    def delete(self,Monitor):
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+    
+    def update(self,Monitor):
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+
+    def select(self,sql):
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+
+
+class Monitoring_Equipment_dao_Impl(base_dao,Monitoring_Equipment_dao):
+    
+    def _init__(self):
+        self.connection = self.get_conn()
+
+    def insert(self,Monitoring_Equipment) :
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+
+    def delete(self,Monitoring_Equipment):
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+    
+    def update(self,Monitoring_Equipment):
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+
+    def select(self,sql):
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+
+
+class Monitoring_Personnel_dao_Impl(base_dao,Monitoring_Personnel_dao):
+    
+    def _init__(self):
+        self.connection = self.get_conn()
+
+    def insert(self,Monitoring_Personnel) :
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+
+    def delete(self,Monitoring_Personnel):
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+    
+    def update(self,Monitoring_Personnel):
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
+
+    def select(self,sql):
+        cursor = self.connection.cursor()
+        #插入sql
+        cursor.execute()
+
+        self.connection.commit()
+        cursor.close()
