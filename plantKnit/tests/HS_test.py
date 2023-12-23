3266d901472d2c6edd1f0a9a536c33a64a2278fd
Xi-Haisen <1184249103@qq.com>
2023-12-23 19:01:20 +0800

    1

--- plantKnit/tests/HS_test.py
+++ plantKnit/tests/HS_test.py
@@ -1,15 +1,17 @@
 import sys
 sys.path.append("../..")
-from db.DaoImpl.daolmpl_X import Monitoring_Personnel_dao_Impl
+from db.DaoImpl.daoImpl_X import Monitoring_Personnel_dao_Impl
 
 
 dao=Monitoring_Personnel_dao_Impl()
 Monitoring_Personnel=dao.select_all()
 
 for job in Monitoring_Personnel:
-    print("JobID:", job[0])
-    print("WorkerID:", job[1])
-    print("养护人：", job[2])
-    print("养护日期：", job[3])
-    print("养护地点：", job[4])
+    print("personID:", job[0])
+    print("姓名:", job[1])
+    print("性别：", job[2])
+    print("出生日期：", job[3])
+    print("电话号码：", job[4])
+    print("创建时间：", job[3])
+    print("更新时间：", job[4])
     print()
