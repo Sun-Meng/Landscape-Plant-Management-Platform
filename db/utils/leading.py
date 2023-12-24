3f9c022af3a53b3d35f44ad39235e3486a2ee85a
Xi-Haisen <1184249103@qq.com>
2023-12-24 20:10:39 +0800

    尝试通用化

--- db/utils/leading.py
+++ db/utils/leading.py
@@ -3,32 +3,35 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append("../..")
 import pandas as pd
-from db.DaoImpl.daoImpl_X import Monitor_dao_Impl, Monitoring_Equipment_dao_Impl, Monitoring_Personnel_dao_Impl
-from db.domain.domain_X import Monitoring_Personnel
 
-def import_csv_to_database(csv_filename):
+def import_csv_to_database(csv_filename, dao_instance, entity_class):
     csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "plantKnit", "data", csv_filename)
     # 读取 CSV 文件
     data = pd.read_csv(csv_path, sep=',')
-    monitor_dao = Monitor_dao_Impl()
-    equipment_dao = Monitoring_Equipment_dao_Impl()
-    personnel_dao = Monitoring_Personnel_dao_Impl()
 
     for _, row in data.iterrows():
-        # 将 Pandas DataFrame 中的行数据转换为列表
         list = row.tolist()
 
-        personnel_data = []
+        entity_data = []
         for item in list:
-            personnel_data.append(item)
+            entity_data.append(item)
 
-        personnel = Monitoring_Personnel(*personnel_data)
-        personnel_dao.insert(personnel)
+        # 使用可变数量的参数传递给实体类的构造函数
+        entity_instance = entity_class(*entity_data)
+        dao_instance.insert(entity_instance)
 
     print(f"CSV文件 '{csv_filename}' 导入数据库成功")
 
 if __name__ == "__main__":
+    from db.DaoImpl.daoImpl_X import Monitor_dao_Impl, Monitoring_Equipment_dao_Impl, Monitoring_Personnel_dao_Impl
+    from db.domain.domain_X import Monitoring_Personnel
+
     # 从终端获取用户输入的 CSV 文件名
     csv_filename = input("请输入要导入的CSV文件的名称（包括文件扩展名）：")
 
-    import_csv_to_database(csv_filename)
+    # 选择要使用的 DAO 实例和实体类
+    dao_instance = Monitoring_Personnel_dao_Impl()  # 可根据需求选择其他实例
+    entity_class = Monitoring_Personnel  # 可根据需求选择其他实体类
+
+    # 调用通用函数
+    import_csv_to_database(csv_filename, dao_instance, entity_class)
