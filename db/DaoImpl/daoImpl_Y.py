deea68101bd5d64223c3e95714f4cb1615a25249
Jwallf <walluend@gmail.com>
2023-12-24 12:09:27 +0800

    测试更改1.0

--- db/DaoImpl/daoImpl_Y.py
+++ db/DaoImpl/daoImpl_Y.py
@@ -1,13 +1,13 @@
 import sys
 sys.path.append("../..")
 from db.Dao.dao_Y import *
-from db.utils.Dao_base import *
+from db.utils.dao import *
 from db.domain.domain_Y import *
-from db.utils.Dao_base import *
 class IllustrationDaoImpl(base_dao,IllustrationDao):
     def __init__(self):
-        self.connection = self.get_conn() 
-        cursor = self.connection.cursor()
+        self.conn = self.get_conn() 
+        self.cursor = self.connection.cursor()
+    
     def insert(self, Illustration:Illustration) -> bool:
         try:
             self.cursor.execute ("INSERT INTO Illustration VALUES (?, ?, ?)",
@@ -256,6 +256,14 @@ class PlantsDaoImpl(base_dao, PlantsDao):
             return Plants(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8])
         else:
             return None
+        
+    def selectAll(self):
+        self.cursor.execute("SELECT * FROM Plants")
+        results = self.cursor.fetchone()
+        simple_informations = []
+        for result in results:
+            simple_informations.append(Plants(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8]))
+        return simple_informations
 
         
 class ShootingDaoImpl(base_dao, ShootingDao):
--- db/utils/Factor.py
+++ db/utils/Factor.py
@@ -1,7 +1,9 @@
 import sys
 
 sys.path.append("../..")
+from db.DaoImpl.daoImpl_S import *
 from db.DaoImpl.daoImpl_Y import *
+from db.DaoImpl.daoImpl_X import *
 class DaoFactory:
     def get_dao(dao_type):
         if dao_type == 'Illustration':
@@ -18,5 +20,14 @@ class DaoFactory:
             return PlantsDaoImpl()
         elif dao_type == 'Shooting':
             return ShootingDaoImpl()
+        
+        elif dao_type == 'CareJob':
+            return careJob_dao_Impl()
+        elif dao_type == 'Medicines':
+            return Monitor_dao_Impl()
+        elif dao_type == 'Prevent':
+            return Monitoring_Equipment_dao_Impl()
+        elif dao_type == 'PestInfo':
+            return Monitoring_Personnel_dao_Impl() 
         else:
             return None
