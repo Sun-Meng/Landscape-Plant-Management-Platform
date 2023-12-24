cf97d83526fa56593ff123a6d2f12c1e9331bc9f
Sun-Meng <sunmeng@bjfu.edu.cn>
2023-12-24 17:33:00 +0800

    sunmeng hand in files with JF examined ok

--- db/domain/domain_S.py
+++ db/domain/domain_S.py
@@ -1,54 +1,54 @@
 class CareJob(object):
     def __init__(self, JobID,WorkerID,Name, Date, Location,RegularJob,Result,create_time,modified_time):
-        self.__JobID = JobID
-        self.__WorkerID = WorkerID
-        self.__Name = Name
-        self.__Date = Date
-        self.__Location = Location
-        self.__RegularJob=RegularJob
-        self.__Result=Result
-        self.__create_time = create_time
-        self.__modified_time=modified_time
+        self.JobID = JobID
+        self.WorkerID = WorkerID
+        self.Name = Name
+        self.Date = Date
+        self.Location = Location
+        self.RegularJob=RegularJob
+        self.Result=Result
+        self.create_time = create_time
+        self.modified_time=modified_time
 
 
 class CareWorker(object):
     def __init__(self, ID, Name, Sex, Birth, Tel,create_time,modified_time):
-        self.__ID = ID
-        self.__Name = Name
-        self.__Sex = Sex
-        self.__Birth = Birth
-        self.__Tel = Tel
-        self.__create_time = create_time
-        self.__modified_time=modified_time
+        self.ID = ID
+        self.Name = Name
+        self.Sex = Sex
+        self.Birth = Birth
+        self.Tel = Tel
+        self.create_time = create_time
+        self.modified_time=modified_time
 
     
 
 class PlantFamily(object):
      def __init__(self, id,name,info):
-        self.__id=id
-        self.__name=name
-        self.__info = info
+        self.id=id
+        self.name=name
+        self.info = info
 
 
 class PlantGenus(object):
      def __init__(self, id,name,info):
-        self.__id=id
-        self.__name=name
-        self.__info = info
+        self.id=id
+        self.name=name
+        self.info = info
 
 
 class PlantSpecies(object):
-    def __init__(self, id,name,info):
-        self.__id=id
-        self.__name=name
-        self.__info = info
+    def __init__(self,id,name,info):
+        self.id=id
+        self.name=name
+        self.info = info
         
 
 class PlantZone(object):
     def __init__(self,ZoneID, Prov, City, County, create_time,modified_time):
-        self.__ZoneID = ZoneID
-        self.__Prov = Prov
-        self.__City = City
-        self.__County = County
-        self.__create_time = create_time
-        self.__modified_time=modified_time
+        self.ZoneID = ZoneID
+        self.Prov = Prov
+        self.City = City
+        self.County = County
+        self.create_time = create_time
+        self.modified_time=modified_time
