1d99e8a1f16bfbd1d5515cee42ae5324e7897369
Xi-Haisen <1184249103@qq.com>
2023-12-24 17:11:17 +0800

    终端选择导入

--- db/utils/leading.py
+++ db/utils/leading.py
@@ -6,12 +6,14 @@ import pandas as pd
 from db.DaoImpl.daoImpl_X import Monitor_dao_Impl, Monitoring_Equipment_dao_Impl, Monitoring_Personnel_dao_Impl
 from db.domain.domain_X import Monitoring_Personnel
 
-def import_csv_to_database(csv_path):
+def import_csv_to_database(csv_filename):
+    csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "plantKnit", "data", csv_filename)
     # 读取 CSV 文件
-    data = pd.read_csv(csv_path,sep=',')
+    data = pd.read_csv(csv_path, sep=',')
     monitor_dao = Monitor_dao_Impl()
     equipment_dao = Monitoring_Equipment_dao_Impl()
     personnel_dao = Monitoring_Personnel_dao_Impl()
+
     for index, row in data.iterrows():
         personnel = Monitoring_Personnel(
             personID=row['personID'],
@@ -23,9 +25,10 @@ def import_csv_to_database(csv_path):
             tel=row['tel']
         )
         personnel_dao.insert(personnel)
-    print("CSV文件导入数据库成功")
+
+    print(f"CSV文件 '{csv_filename}' 导入数据库成功")
 
 if __name__ == "__main__":
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    csv_path = os.path.join("..\..\plantKnit\data\HS.csv")
-    import_csv_to_database(csv_path)
+    # 从终端获取用户输入的 CSV 文件名
+    csv_filename = input("请输入要导入的CSV文件的名称（包括文件扩展名）：")
+    import_csv_to_database(csv_filename)
